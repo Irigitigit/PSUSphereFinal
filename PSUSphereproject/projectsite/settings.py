cddef2d7cd1e40d1adfd2df9a340370b1ddcf297
unknown <202380079@psu.palawan.edu.ph>
2026-03-08 19:10:12 +0800

    Done C and D

--- PSUSphereproject/projectsite/settings.py
+++ PSUSphereproject/projectsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-o=5js@4ny$bf&tbcu!ozcg8ecipm@%p13mo^v&)ttb*h-=5+hs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mikopatindol1.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['mikopatindol1.pythonanywhere.com','127.0.0.1','localhost']
 
 
 # Application definition
@@ -48,7 +49,11 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
-SITE_ID = 2
+if "pythonanywhere" in socket.gethostname():
+    SITE_ID = 1 # production site (psusphere.pythonanywhere.com)
+else:
+    SITE_ID = 2 # local site (127.0.0.1:8000)
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
